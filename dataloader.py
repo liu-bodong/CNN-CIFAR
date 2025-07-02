454f50c5815821419858ce7b2468f0cd844f4aec
liubodong <liubodong04@gmail.com>
2025-07-02 10:10:12 +0800

    Delete notes.ipynb and add download_data() function in dataloader.py for downloading data.

--- dataloader.py
+++ dataloader.py
@@ -4,17 +4,29 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 
-def load_data(batch_size=256, num_workers=4):
+def load_data(download=False, batch_size=256, num_workers=4):
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
 
-    train_data = torchvision.datasets.CIFAR10(root='./data', train=True,  download=False, transform=transform)
-    test_data  = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
+    train_data = torchvision.datasets.CIFAR10(root='./data', train=True,  download=download, transform=transform)
+    test_data  = torchvision.datasets.CIFAR10(root='./data', train=False, download=download, transform=transform)
 
     train_loader = DataLoader(train_data, batch_size=4, shuffle=True,  num_workers=4)
     test_loader  = DataLoader(test_data,  batch_size=4, shuffle=False, num_workers=4)
     
     return train_loader, test_loader
 
+def download_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    torchvision.datasets.CIFAR10(root='./data', train=True,  download=True, transform=transform)
+    torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+if __name__ == "__main__":
+    print("Download data from external source")
+    download_data()
